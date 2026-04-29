a995146cee899037693bb376fbddafedba645106
sinsin-morning <0731oshin@gmail.com>
2026-04-30 05:12:59 +0900

    Add Vercel config API endpoint

---
+++ api/config.js
@@ -0,0 +1,7 @@
+export default function handler(req, res) {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.status(200).json({
+    url: process.env.SUPABASE_URL,
+    anonKey: process.env.SUPABASE_ANON_KEY,
+  });
+}
